9fa7c242b32dc7298a4b57d4297b07d8a93e6108
pavelnil <pavelmqx@gmail.com>
2025-08-07 01:18:53 +0300

    Delete public directory

--- public/hfs-speedtest.js
+++
@@ -1,6 +0,0 @@
-﻿'use strict';
-document.addEventListener('DOMContentLoaded', () => {
-    // This is used for the global frontend for HFS (across all pages).
-    // This might be useful in the future.
-    // For now, everything is in a single speedtest.html.
-});
